df24be1b1acfacacfd2526b88f14962fe0ced4a6
AMOREM <amorem@amorem.com>
2019-07-27 19:52:52 +0200

    Loading spinner

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
-import 'package:clima/services/networking.dart' as prefix0;
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = "b534ac4878391a3c0db3305f54a42640";
 
@@ -30,10 +31,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
         "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey");
 
     var weatherData = await networkHelper.getData();
+
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LocationScreen()));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.00,
+        ),
+      ),
+    );
   }
 }
