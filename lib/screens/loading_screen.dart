498ae31b18c95050965e90a30d62d23045a3e899
AMOREM <amorem@amorem.com>
2019-07-27 17:54:36 +0200

    using registered openweather api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = "b534ac4878391a3c0db3305f54a42640";
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,23 +11,34 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   initState() {
     super.initState();
     getLocation();
-    getData();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+    getData();
   }
 
   void getData() async {
     http.Response response = await http.get(
-        "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22");
+        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey");
     if (response.statusCode == 200) {
       String data = response.body;
+      var decodedData = jsonDecode(data);
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+      print(
+          cityName.toString() + condition.toString() + temperature.toString());
     } else {
       print(response.statusCode);
     }
